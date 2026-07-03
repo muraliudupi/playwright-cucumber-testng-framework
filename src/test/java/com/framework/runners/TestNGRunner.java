f88535d1fde2702ceb20ffb3df1991304984a6ec
Murali <muralikrishna.udupi@outlook.com>
2026-07-03 12:00:47 +0530

    ci: fix cucumber plugin array syntax alignment by removing trailing colon

--- src/test/java/com/framework/runners/TestNGRunner.java
+++ src/test/java/com/framework/runners/TestNGRunner.java
@@ -10,13 +10,14 @@ import org.testng.annotations.DataProvider;
         tags = "not @ignore",
         plugin = {
                 "pretty",
-                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter",
                 "html:build/reports/cucumber/cucumber-report.html",
                 "json:build/reports/cucumber/cucumber-report.json"
         },
         monochrome = true
 )
 public class TestNGRunner extends AbstractTestNGCucumberTests {
+
         @Override
         @DataProvider(parallel = true)
         public Object[][] scenarios() {
